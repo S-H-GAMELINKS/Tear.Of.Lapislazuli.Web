704df36e563ebc37d1fc6d20a3d5032f7128bbd9
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-01 23:35:59 +0900

    ヘッダー＆フッターのpath 設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'tear_of_lapislazuli#index'
-  get 'tear_of_lapislazuli/about'
-  get 'tear_of_lapislazuli/character'
-  get 'tear_of_lapislazuli/download'
-  get 'tear_of_lapislazuli/link'
+  get '/about', to: 'tear_of_lapislazuli#about'
+  get '/character', to: 'tear_of_lapislazuli#character'
+  get '/download', to: 'tear_of_lapislazuli#download'
+  get '/link', to: 'tear_of_lapislazuli#link'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
